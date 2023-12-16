307bc2f6a87d7ce6552ff73cb58405ac80fb75d9
harishaariga <134035423+harishaariga@users.noreply.github.com>
2023-12-16 09:30:37 +0530

    Add files via upload

---
+++ home.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var typed = new Typed(".text", {
+      strings: ["Frontend Developer", "Web Developer"],
+      typeSpeed: 100,
+      backSpeed: 50,
+      backDelay: 1000,
+      loop: true
+    });
+  });
+  
+ 
